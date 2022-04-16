26e08751deead4f0de263505a6e82eb329799844
Shaan <shaan.erickson@chicagobooth.edu>
2022-04-16 11:06:43 -0500

    First test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,40 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+  def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+    render({ :template => "game_templates/rock.html.erb", :layout => "wrapper.html.erb"})
+  end
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+    render({ :template => "game_templates/paper.html.erb", :layout => "wrapper.html.erb"})
+  end
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    render({ :template => "game_templates/scissors.html.erb", :layout => "wrapper.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-
-
+  self.get("", { :controller => "application", :action => "homepage"})
+  self.get("/rock", { :controller => "application", :action => "play_rock" })
+  self.get("/paper", { :controller => "application", :action => "play_paper"})
+  self.get("/scissors", { :controller => "application", :action => "play_scissors"})
 end
